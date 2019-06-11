263de8559e8277773008aac7b6a0a56c7e938ea3
yeebaplay <vinicius.parise@hotmail.com>
2019-06-11 15:42:30 -0300

    up

--- lib/Iugu/Withdraw.php
+++ lib/Iugu/Withdraw.php
@@ -6,4 +6,33 @@ class Iugu_Withdraw extends APIResource
     {
         return self::createAPI($attributes);
     }
+    
+    public static function fetch($key)
+    {
+        return self::fetchAPI($key);
+    }
+    
+    public function withdraw()
+    {
+        if ($this->is_new()) {
+            return false;
+        }
+        
+        try {
+            $response = self::API()->request(
+                'PUT',
+                static::url($this).'/request_withdraw'
+            );
+            if (isset($response->errors)) {
+                throw new IuguRequestException($response->errors);
+            }
+            $new_object = self::createFromResponse($response);
+            $this->copy($new_object);
+            $this->resetStates();
+        } catch (Exception $e) {
+            return false;
+        }
+        
+        return true;
+    }
 }
